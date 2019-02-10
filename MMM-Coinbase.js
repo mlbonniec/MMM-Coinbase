b34e4f4026868ea4da2074ee1b51f2c2b4a076e8
Mathis Le Bonniec <mathislbonniec@gmail.com>
2019-02-10 19:28:19 +0100

    Add files via upload

---
+++ MMM-Coinbase.js
@@ -0,0 +1,43 @@
+Module.register("MMM-Coinbase", {
+    
+    defaults: {
+        apiKey: "",
+        apiSecret: "",
+        // updateInterval: 1
+    },
+    
+    getStyles: function () {
+        return [this.file('css/styles.css')];
+    },
+
+    start: function () {},
+    
+    getDom: function() {
+        var element = document.createElement("div")
+        element.id = "bitcoinWallet"
+        element.innerHTML = "<i class='fab fa-bitcoin'></i> "
+        var subElement = document.createElement("p")
+        subElement.id = "bitcoinCounter"
+        element.appendChild(subElement)
+        return element
+    },
+      
+    notificationReceived: function(notification, payload, sender) {
+        switch(notification) {
+            case "DOM_OBJECTS_CREATED":
+                var timer = setInterval(()=>{
+                    this.sendSocketNotification("DO_YOUR_JOB", {'apiKey': this.config.apiKey, 'apiSecret': this.config.apiSecret})
+                }, 5000)
+            break
+        }
+    },
+      
+    socketNotificationReceived: function(notification, payload) {
+        switch(notification) {
+            case "I_DID":
+                var elem = document.getElementById("bitcoinCounter")
+                elem.innerHTML = payload
+            break
+        }
+    },
+})
---
+++ node_helper.js
@@ -0,0 +1,23 @@
+var NodeHelper = require("node_helper");
+var Client = require('coinbase').Client;
+
+var bal;
+
+module.exports = NodeHelper.create({
+    start: function() {
+        bal = 0;
+    },
+    socketNotificationReceived: function(notification, payload) {
+        var client = new Client({'apiKey': payload.apiKey, 'apiSecret': payload.apiSecret});
+        switch(notification) {
+            case "DO_YOUR_JOB":
+                client.getAccounts({}, function(err, accounts) {
+                    accounts.forEach(function(acct) {
+                        bal=acct.balance.amount;
+                    });
+                });
+                this.sendSocketNotification("I_DID", bal)
+            break
+        }
+    },
+})
