7e41abbde2b7325c30caedd2032e215bd62dc471
nabbl <dakappler@gmail.com>
2019-07-31 12:57:55 +0200

    implement support for multiple currencies shows balance in header

--- node_helper.js
+++ node_helper.js
@@ -1,21 +1,17 @@
 var NodeHelper = require("node_helper");
-var Client = require('coinbase').Client;
-
-var bal;
+var Client = require("coinbase").Client;
 
 module.exports = NodeHelper.create({
-    start: function() {
-        bal = 0;
-    },
-    socketNotificationReceived: function(notification, payload) {
-        var client = new Client({'apiKey': payload.apiKey, 'apiSecret': payload.apiSecret});
-        switch(notification) {
-            case "DO_YOUR_JOB":
-                client.getAccounts({}, function(err, accounts) {
-                    bal = accounts[0].balance.amount;
-                });
-                this.sendSocketNotification("I_DID", bal)
-            break
-        }
-    },
-})
+	start: function() {},
+	socketNotificationReceived: function(notification, payload) {
+		var client = new Client({"apiKey": payload.apiKey, "apiSecret": payload.apiSecret});
+		switch(notification) {
+		case "DO_YOUR_JOB":
+			var helper = this;
+			client.getAccounts({}, function(err, accounts) {
+				helper.sendSocketNotification("I_DID", accounts);
+			});
+			break;
+		}
+	},
+});
